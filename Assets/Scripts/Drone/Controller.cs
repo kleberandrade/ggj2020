11f00cd97cd9478480a14d2cd4457e8111304503
JoaoColeoni <JoaoColeoni@users.noreply.github.com>
2020-02-01 07:44:38 -0300

    Camera Atualização

--- Assets/Scripts/Drone/Controller.cs
+++ Assets/Scripts/Drone/Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Controller : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Controller : MonoBehaviour
     public Drop DropScript;
     public Pick PickScript;
     public Spawn SpawnScript;
+    public Transform SpawnTransform;
+    public CinemachineVirtualCamera Cinemachine;
     public int Itens = 0;
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,8 @@ public class Controller : MonoBehaviour
         Invoke("RemoveCooldown",2);
         Destroy(PickScript);
         DropScript.DropDeath();
+        Cinemachine.Follow = SpawnTransform;
+        Cinemachine.LookAt = SpawnTransform;
     }
 
     void RemoveCooldown(){
