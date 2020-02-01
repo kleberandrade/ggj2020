88da7f7bbd214d463a089abbe06002bfc8576d6d
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-02-01 14:28:29 -0300

    MovePingPong created

---
+++ Assets/Scripts/UIs/MovePingPongUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MovePingPongUI : MonoBehaviour
+{
+    public float m_Range = 0.05f;
+    public float m_SmoothTime = 10.0f;
+    public Vector3 m_Axis = Vector3.up;
+
+    private Vector3 m_StartPosition;
+
+    private void Start()
+    {
+        m_StartPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = m_StartPosition + m_Axis * Mathf.Sin(Time.time * m_SmoothTime) * m_Range;
+    }
+}
