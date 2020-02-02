126f8a514ea62173738a8ed75a4b4235637df753
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-02-02 05:09:59 -0300

    Update HealthBar.cs

--- Assets/Scripts/UIs/HealthBar.cs
+++ Assets/Scripts/UIs/HealthBar.cs
@@ -4,52 +4,51 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public enum HealthBarState { None, Increase, Decrease }
+
     [Header("UI")]
     public Image m_Bar;
-    public Text m_Label;
 
     [Header("Settings")]
-    public float m_MaxValue;
+    public float m_LifeTime;
+    public float m_CooldownTime;
 
-    [Header("Time")]
-    public bool m_UseTime;
-    public float m_FillTime;
+    private float m_StartTime;
+    private HealthBarState m_State = HealthBarState.None;
 
-    private float m_Value;
-    private float m_ElapsedTime;
-
-    private void Start()
+    public void Play()
     {
-        UpdateHealth(m_MaxValue);
+        m_StartTime = Time.time;
+        m_State = HealthBarState.Increase;
     }
 
-    public void SetIncrementalValue(float value) {
-        if (m_UseTime){
-            StartCoroutine(UpdateHealthTime(m_Value, Mathf.Clamp(m_Value + value, 0.0f, m_MaxValue)));
-        } else {
-            UpdateHealth(value);
-        }
+    public void Stop()
+    {
+        m_StartTime = Time.time;
+        m_State = HealthBarState.Decrease;
     }
 
-    private void UpdateHealth(float value)
+    public void Update()
     {
-        m_Value = Mathf.Clamp(m_Value + value, 0.0f, m_MaxValue);
-        m_Bar.fillAmount = m_Value / m_MaxValue;
-
-        if (m_Label)
-            m_Label.text = string.Format("{0:0} / {1:0}", m_Value, m_MaxValue);
-    }
-
-    private IEnumerator UpdateHealthTime(float fromValue, float toValue)
-    {
-        m_ElapsedTime = 0.0f;
-        while(m_ElapsedTime / m_FillTime < 1.0f)
+        if (m_State == HealthBarState.Increase)
         {
-            m_ElapsedTime += Time.deltaTime;
-            m_Value = Mathf.Lerp(fromValue, toValue, m_ElapsedTime / m_FillTime);
-            m_Bar.fillAmount = m_Value / m_MaxValue;
-            m_Label.text = string.Format("{0:0} / {1:0}", m_Value, m_MaxValue);
-            yield return null;
+            float value = (Time.time - m_StartTime) / m_LifeTime;
+            Debug.Log($"Increase {value}");
+            m_Bar.fillAmount = 1.0f - value;
+
+            if (value >= 1.0f)
+                Stop();
+        }
+
+        if (m_State == HealthBarState.Decrease)
+        {
+            float value = (Time.time - m_StartTime) / m_CooldownTime;
+            m_Bar.fillAmount = value;
+
+            if (value >= 1.0f)
+            {
+                m_State = HealthBarState.None;
+            }
         }
     }
 }
