549f9f316912b1c927eae24bd0badd8e8dfea2c1
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-02-02 15:08:25 -0300

    Gameover screen

---
+++ Assets/Scripts/Managers/Gameover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class Gameover : MonoBehaviour
+{
+    public GameObject[] m_Trophys;
+
+    public static Ranking m_Ranking;
+
+    private void Start()
+    {
+        for (int i = 0; i < m_Trophys.Length; i++)
+        {
+            m_Trophys[i].SetActive(false);
+        }
+    }
+
+    public void Winner()
+    {
+        User user = m_Ranking.Users.OrderByDescending(x => x.Gears).First<User>();
+        m_Trophys[user.Id].SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("XboxOneButtonA1") || Input.GetButtonDown("XboxOneButtonA2") || Input.GetButtonDown("XboxOneButtonA3") || Input.GetButtonDown("XboxOneButtonA4"))
+        {
+            ScreenManager.Instance.LoadLevel("MainMenu");
+        }
+    }
+}
